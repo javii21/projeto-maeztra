94c4260622ff69406a4add584715bba884f57386
Javier Romero <loljavieralexander@gmail.com>
2023-01-23 23:08:57 -0300

    project initiation maeztra, creation newsletter

---
+++ src/cliente.js
@@ -0,0 +1,69 @@
+const urlimg = "";
+const root = document.querySelector('#rootElement');
+
+root && root.insertAdjacentHTML('afterbegin', `
+<div 
+  class="WrapperNewsletter">
+  
+  <div 
+    class="box">
+  
+    <a 
+    class="buttonModalClose"  
+    href="#"
+    >
+    Fechar
+  </a>  
+  
+  <img 
+    class="img" src="./src/images/image04.jpg" 
+    alt="imgan da newsletter"
+  />
+
+  <form 
+    class="form"> 
+    
+      <img 
+        class="form__img" 
+        src="./src/assets/newsletter-mail-icon.svg"
+        alt="icone da newsletter email" 
+      />
+    
+      <p 
+        class="form__paragraph"
+        >
+        bem vindo a maeztra
+      </p>
+    
+      <h2 
+        class="form__titleDiscounts">
+        Receba em Primeira mão
+        
+        <strong>
+          desconto e ofertas exclusivas
+        </strong>
+      </h2>
+
+      <input 
+        class="form__inputEmail" 
+        type="email" name="email" 
+        placeholder="Digite seu e-mail"
+      />
+
+      <button 
+        class="form__button">
+      
+        enviar
+        
+        <img 
+        src="/src/assets/normal-icon-left.svg" 
+        alt="icono de enviar"
+        />
+
+      </button>
+
+    </form>
+  </div>
+
+</div>
+`);
